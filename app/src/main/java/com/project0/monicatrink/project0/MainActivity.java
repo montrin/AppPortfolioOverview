4a2a6a9ef3314b5fcde5acc3c460d93c641cc130
Monica Trink <monica.trink@gmail.com>
2015-07-13 15:25:46 +0200

    added Toasts

--- app/src/main/java/com/project0/monicatrink/project0/MainActivity.java
+++ app/src/main/java/com/project0/monicatrink/project0/MainActivity.java
@@ -1,24 +1,49 @@
 package com.project0.monicatrink.project0;
 
+import android.app.Activity;
 import android.content.Context;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-
-public class MainActivity extends FragmentActivity {
-
-
-
-    public void sendMessage() {
+public class MainActivity extends Activity {
+    final String STD_MESSAGE = "This button will launch my ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void createToast(String message) {
         Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
+        CharSequence text = message;
         int duration = Toast.LENGTH_SHORT;
-
         Toast.makeText(context, text, duration).show();
+    }
 
+    public void showToastbtnSpotify(View view) {
+        final Button button = (Button) findViewById(R.id.btnSpotify);
+        createToast(STD_MESSAGE + button.getText() + " app");
+    }
+
+    public void showToastbtnScores(View view) {
+        final Button button = (Button) findViewById(R.id.btnScores);
+        createToast(STD_MESSAGE + button.getText() + " app");
+    }
+    public void showToastbtnLib(View view) {
+        final Button button = (Button) findViewById(R.id.btnLib);
+        createToast(STD_MESSAGE + button.getText() + " app");
+    }
+    public void showToastbtnBigger(View view) {
+        final Button button = (Button) findViewById(R.id.btnBigger);
+        createToast(STD_MESSAGE + button.getText() + " app");
+    }
+    public void showToastbtnXYZ(View view) {
+        final Button button = (Button) findViewById(R.id.btnXYZ);
+        createToast(STD_MESSAGE + button.getText() + " app");
+    }
+    public void showToastbtnCap(View view) {
+        final Button button = (Button) findViewById(R.id.btnCap);
+        createToast(STD_MESSAGE + button.getText() + " app");
     }
 }
